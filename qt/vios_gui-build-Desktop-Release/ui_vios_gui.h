6d2f418233ea418e659c28eab07e61440910aff2
jrajeev <rajeev.geni@gmail.com>
2012-12-08 00:52:32 -0500

    Added extra buttons

--- qt/vios_gui-build-Desktop-Release/ui_vios_gui.h
+++ qt/vios_gui-build-Desktop-Release/ui_vios_gui.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'vios_gui.ui'
 **
-** Created: Thu Dec 6 15:55:45 2012
+** Created: Fri Dec 7 16:10:16 2012
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -15,10 +15,12 @@
 #include <QtGui/QApplication>
 #include <QtGui/QButtonGroup>
 #include <QtGui/QCheckBox>
+#include <QtGui/QDoubleSpinBox>
 #include <QtGui/QGraphicsView>
 #include <QtGui/QGroupBox>
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
+#include <QtGui/QListView>
 #include <QtGui/QListWidget>
 #include <QtGui/QMainWindow>
 #include <QtGui/QMenu>
@@ -27,7 +29,6 @@
 #include <QtGui/QRadioButton>
 #include <QtGui/QSlider>
 #include <QtGui/QStatusBar>
-#include <QtGui/QTableView>
 #include <QtGui/QTextEdit>
 #include <QtGui/QToolBar>
 #include <QtGui/QToolButton>
@@ -48,10 +49,21 @@ public:
     QLabel *label_2;
     QLabel *label;
     QCheckBox *checkBox;
-    QTableView *tableView;
+    QListView *listView;
+    QLabel *label_3;
+    QDoubleSpinBox *doubleSpinBox;
+    QDoubleSpinBox *doubleSpinBox_2;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton_3;
+    QRadioButton *radioButton_4;
+    QLabel *label_7;
     QGroupBox *face_label;
     QRadioButton *radioButton;
     QTextEdit *textEdit_2;
+    QToolButton *toolButton;
     QGroupBox *menu;
     QPushButton *prev_skip_button;
     QPushButton *prev_button;
@@ -118,7 +130,7 @@ public:
         selected_images->setAlignment(Qt::AlignHCenter|Qt::AlignTop);
         graphicsView_detection = new QGraphicsView(selected_images);
         graphicsView_detection->setObjectName(QString::fromUtf8("graphicsView_detection"));
-        graphicsView_detection->setGeometry(QRect(10, 70, 155, 155));
+        graphicsView_detection->setGeometry(QRect(10, 50, 155, 155));
         QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
         sizePolicy1.setHorizontalStretch(0);
         sizePolicy1.setVerticalStretch(0);
@@ -128,24 +140,66 @@ public:
         graphicsView_detection->setMaximumSize(QSize(16777215, 16777215));
         graphicsView_processed = new QGraphicsView(selected_images);
         graphicsView_processed->setObjectName(QString::fromUtf8("graphicsView_processed"));
-        graphicsView_processed->setGeometry(QRect(200, 110, 70, 70));
+        graphicsView_processed->setGeometry(QRect(190, 80, 70, 70));
         sizePolicy1.setHeightForWidth(graphicsView_processed->sizePolicy().hasHeightForWidth());
         graphicsView_processed->setSizePolicy(sizePolicy1);
         label_2 = new QLabel(selected_images);
         label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(190, 80, 91, 17));
+        label_2->setGeometry(QRect(180, 60, 101, 17));
+        QFont font;
+        font.setBold(true);
+        font.setWeight(75);
+        label_2->setFont(font);
         label = new QLabel(selected_images);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(40, 40, 101, 17));
+        label->setGeometry(QRect(40, 30, 101, 17));
+        label->setFont(font);
         checkBox = new QCheckBox(selected_images);
         checkBox->setObjectName(QString::fromUtf8("checkBox"));
-        checkBox->setGeometry(QRect(20, 240, 121, 22));
-        tableView = new QTableView(selected_images);
-        tableView->setObjectName(QString::fromUtf8("tableView"));
-        tableView->setGeometry(QRect(20, 280, 181, 81));
+        checkBox->setGeometry(QRect(10, 210, 121, 22));
+        listView = new QListView(selected_images);
+        listView->setObjectName(QString::fromUtf8("listView"));
+        listView->setGeometry(QRect(10, 260, 161, 111));
+        label_3 = new QLabel(selected_images);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(60, 240, 66, 17));
+        doubleSpinBox = new QDoubleSpinBox(selected_images);
+        doubleSpinBox->setObjectName(QString::fromUtf8("doubleSpinBox"));
+        doubleSpinBox->setGeometry(QRect(180, 190, 61, 21));
+        QFont font1;
+        font1.setPointSize(11);
+        doubleSpinBox->setFont(font1);
+        doubleSpinBox_2 = new QDoubleSpinBox(selected_images);
+        doubleSpinBox_2->setObjectName(QString::fromUtf8("doubleSpinBox_2"));
+        doubleSpinBox_2->setGeometry(QRect(180, 220, 61, 21));
+        doubleSpinBox_2->setFont(font1);
+        label_4 = new QLabel(selected_images);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(250, 190, 16, 17));
+        label_4->setFont(font1);
+        label_5 = new QLabel(selected_images);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(250, 220, 16, 17));
+        label_6 = new QLabel(selected_images);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(200, 170, 51, 17));
+        label_6->setFont(font);
+        radioButton_2 = new QRadioButton(selected_images);
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
+        radioButton_2->setGeometry(QRect(180, 290, 101, 22));
+        radioButton_3 = new QRadioButton(selected_images);
+        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
+        radioButton_3->setGeometry(QRect(180, 310, 91, 22));
+        radioButton_4 = new QRadioButton(selected_images);
+        radioButton_4->setObjectName(QString::fromUtf8("radioButton_4"));
+        radioButton_4->setGeometry(QRect(180, 330, 91, 22));
+        label_7 = new QLabel(selected_images);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(180, 270, 101, 20));
+        label_7->setFont(font);
         face_label = new QGroupBox(centralWidget);
         face_label->setObjectName(QString::fromUtf8("face_label"));
-        face_label->setGeometry(QRect(660, 440, 301, 171));
+        face_label->setGeometry(QRect(660, 440, 301, 191));
         face_label->setStyleSheet(QString::fromUtf8("QGroupBox {\n"
 "     background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
 "                                       stop: 0 #E0E0E0, stop: 1 #FFFFFF);\n"
@@ -167,6 +221,9 @@ public:
         textEdit_2 = new QTextEdit(face_label);
         textEdit_2->setObjectName(QString::fromUtf8("textEdit_2"));
         textEdit_2->setGeometry(QRect(160, 20, 121, 131));
+        toolButton = new QToolButton(face_label);
+        toolButton->setObjectName(QString::fromUtf8("toolButton"));
+        toolButton->setGeometry(QRect(190, 150, 61, 31));
         menu = new QGroupBox(centralWidget);
         menu->setObjectName(QString::fromUtf8("menu"));
         menu->setGeometry(QRect(10, 540, 631, 80));
@@ -243,8 +300,17 @@ public:
         label_2->setText(QApplication::translate("ViosGui", "Cropped Face", 0, QApplication::UnicodeUTF8));
         label->setText(QApplication::translate("ViosGui", "Detected Face", 0, QApplication::UnicodeUTF8));
         checkBox->setText(QApplication::translate("ViosGui", "Show Margins", 0, QApplication::UnicodeUTF8));
+        label_3->setText(QApplication::translate("ViosGui", "Statistics", 0, QApplication::UnicodeUTF8));
+        label_4->setText(QApplication::translate("ViosGui", "H", 0, QApplication::UnicodeUTF8));
+        label_5->setText(QApplication::translate("ViosGui", "V", 0, QApplication::UnicodeUTF8));
+        label_6->setText(QApplication::translate("ViosGui", "Indent", 0, QApplication::UnicodeUTF8));
+        radioButton_2->setText(QApplication::translate("ViosGui", "Both Eyes", 0, QApplication::UnicodeUTF8));
+        radioButton_3->setText(QApplication::translate("ViosGui", "One Eye", 0, QApplication::UnicodeUTF8));
+        radioButton_4->setText(QApplication::translate("ViosGui", "No Eyes", 0, QApplication::UnicodeUTF8));
+        label_7->setText(QApplication::translate("ViosGui", "Detect only if", 0, QApplication::UnicodeUTF8));
         face_label->setTitle(QApplication::translate("ViosGui", "Face Label", 0, QApplication::UnicodeUTF8));
         radioButton->setText(QApplication::translate("ViosGui", "RadioButton", 0, QApplication::UnicodeUTF8));
+        toolButton->setText(QApplication::translate("ViosGui", "Browse", 0, QApplication::UnicodeUTF8));
         menu->setTitle(QApplication::translate("ViosGui", "menu", 0, QApplication::UnicodeUTF8));
         prev_skip_button->setText(QApplication::translate("ViosGui", "<<", 0, QApplication::UnicodeUTF8));
         prev_button->setText(QApplication::translate("ViosGui", "<", 0, QApplication::UnicodeUTF8));
